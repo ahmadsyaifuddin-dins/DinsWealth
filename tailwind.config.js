e116411d08a2969e84ef867d860285ab1a350b66
Ahmad Syaifuddin <77381720+ahmadsyaifuddin-dins@users.noreply.github.com>
2025-07-20 11:56:06 +0800

    Add Tailwind CSS and update Vite config
    Integrated Tailwind CSS by adding tailwindcss and related plugins to dependencies, creating a tailwind.config.js, and updating Vite configuration. Modified app.css and welcome.blade.php to use Tailwind utility classes. Upgraded Vite and related build dependencies to latest versions.

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+export default {
+    content: [
+      './resources/views/**/*.blade.php',
+      './resources/js/**/*.{js,vue,ts,jsx,tsx}',
+    ],
+    theme: { extend: {} },
+    plugins: [],
+  };
+  
